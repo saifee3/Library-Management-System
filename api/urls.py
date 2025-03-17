625447d81f09fd867fac811d350d95ed65fbd9cb
Saif <185240419+saifee3@users.noreply.github.com>
2025-03-17 21:09:35 +0500

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,30 +1,10 @@
 from django.urls import path
-from .views import *
+from .views import   AuthorAPIView, BookAPIView, BorrowReturnBookAPIView, LibraryAPIView, UserAPIView
 
-# Group URLs by functionality and user roles
 urlpatterns = [
-    path('auth/register/', register_user),
-    path('auth/login/', login_user),
-    
-    path('search-filter/', book_search,),
-
-    path('authors/create/', author_create), 
-    path('authors/', author_list),                                          
-    path('authors/<id>/', get_author),                 
-    path('authors/<id>/update/', update_author),    
-    path('authors/<id>/delete/', delete_author),       
-    
-    path('books/create/', book_create),
-    path('books/borrow/', borrow_book),          
-    path('books/return/', return_book),  
-    path('books/', book_list),
-    path('books/<id>/', get_book),       
-    path('books/<id>/update/', update_book),
-    path('books/<id>/delete/', delete_book),                               
-
-    path('library/statistics/', library_statistics),
-    
-    path('borrowers/', borrower_list),     
-
-    
+    path('user/<action>/', UserAPIView.as_view()),
+    path('authors/<id>/', AuthorAPIView.as_view()),
+    path('books/<id>/', BookAPIView.as_view()),
+    path('borrowing/<action>/', BorrowReturnBookAPIView.as_view()),
+    path('library/<action>/', LibraryAPIView.as_view()),
 ]
